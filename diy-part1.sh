12b08b6770643ed1b5052163ca0839799ff2a492
Y0J1G3N <43652659+Y0J1G3N@users.noreply.github.com>
2021-02-26 21:18:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,10 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/Y0J1G3N/luci-app-poweroff
-sed -i "2i uci set network.lan.ifname='eth0'" package/lean/default-settings/files/zzz-default-setting
-sed -i "3i uci set network.lan.proto='static'" package/lean/default-settings/files/zzz-default-settings
-sed -i "4i uci set network.lan.ipaddr='10.0.0.2'" package/lean/default-settings/files/zzz-default-settings
-sed -i "5i uci set network.lan.netmask='255.255.255.128'" package/lean/default-settings/files/zzz-default-settings 
-sed -i "6i uci set network.lan.gateway='10.0.0.1'" package/lean/default-settings/files/zzz-default-settings
-sed -i "7i uci set network.lan.dns='10.0.0.2'" package/lean/default-settings/files/zzz-default-settings
-sed -i "8i uci set network.lan.delegate='0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "2i uci set network.lan.ifname='eth0'" lean/default-settings/files/zzz-default-setting
+sed -i "3i uci set network.lan.proto='static'" lean/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.lan.ipaddr='10.0.0.2'" lean/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.lan.netmask='255.255.255.128'" lean/default-settings/files/zzz-default-settings 
+sed -i "6i uci set network.lan.gateway='10.0.0.1'" lean/default-settings/files/zzz-default-settings
+sed -i "7i uci set network.lan.dns='10.0.0.2'" lean/default-settings/files/zzz-default-settings
+sed -i "8i uci set network.lan.delegate='0'" lean/default-settings/files/zzz-default-settings
