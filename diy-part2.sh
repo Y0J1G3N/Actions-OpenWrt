30dcdd4e6205e9be4559604f69d3fe52b920bcd4
Y0J1G3N <43652659+Y0J1G3N@users.noreply.github.com>
2021-02-26 21:36:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,10 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i "2i uci set network.lan.ifname='eth0'" openwrt/package/lean/default-settings/files/zzz-default-setting
-sed -i "3i uci set network.lan.proto='static'" openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i "4i uci set network.lan.ipaddr='10.0.0.2'" openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i "5i uci set network.lan.netmask='255.255.255.128'" openwrt/package/lean/default-settings/files/zzz-default-settings 
-sed -i "6i uci set network.lan.gateway='10.0.0.1'" openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i "7i uci set network.lan.dns='10.0.0.2'" openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i "8i uci set network.lan.delegate='0'" openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "2i uci set network.lan.ifname='eth0'" package/lean/default-settings/files/zzz-default-settings
+sed -i "3i uci set network.lan.proto='static'" package/lean/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.lan.ipaddr='10.0.0.2'" package/lean/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.lan.netmask='255.255.255.128'" package/lean/default-settings/files/zzz-default-settings
+sed -i "6i uci set network.lan.gateway='10.0.0.1'" package/lean/default-settings/files/zzz-default-settings
+sed -i "7i uci set network.lan.dns='10.0.0.2'" package/lean/default-settings/files/zzz-default-settings
+sed -i "8i uci set network.lan.delegate='0'" package/lean/default-settings/files/zzz-default-settings
